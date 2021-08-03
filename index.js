5571adea9a407b84c47a59473e9eea5778c06fb7
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-08-03 08:09:52 +0800

    簡化程式碼

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const list = document.querySelector("#my-todo");
 const addBtn = document.querySelector("#add-btn");
 const input = document.querySelector("#new-todo");
 const finshList = document.querySelector('#finsh-todo')
+const allTodo = document.querySelector('.all-todo')
 
 // 資料
 let todos = [
@@ -20,56 +21,46 @@ for (let todo of todos) {
 // Create (按按鈕輸入)
 addBtn.addEventListener("click", function () {
   const inputValue = input.value;
-
   if (checktext (inputValue)) {
     addItem(inputValue);
   }
 });
 
 //按Enter輸入
-input.addEventListener("keydown", function () {
+input.addEventListener("keydown", function (event) {
   const inputValue = input.value ;
   if (event.which === 13 && checktext (inputValue)) {
     addItem(inputValue);
   }
 });
 
-// 待辦清單Delete and check
-list.addEventListener("click", function (event) {
+// Todo Delete and check
+allTodo.addEventListener("click", function (event) {
   const target = event.target;
-
-  if (target.classList.contains("delete")) {
-    const parentElement = target.parentElement;
-    parentElement.remove();
-  } else if (target.tagName === "LABEL") {
-    const text = target.innerText
-    addFinshItem(text)
-    target.parentElement.remove();
+  const parent = target.parentElement;
+  if (target.classList.contains("delete")) {  //按到刪除
+    parent.remove();
+  } else if (target.tagName === "LABEL") {   //按到代辦清單
+    const grandfather = parent.parentElement
+    if(grandfather.classList.contains('my-todo')) {   //按到未完成清單
+       target.classList.add('checked')
+       finshList.appendChild(parent)
+    } else {                                          //按到完成清單
+        list.appendChild(parent)
+        target.classList.remove('checked')
+    }  
   }
 });
 
-//編輯已完成代辦清單
-finshList.addEventListener('click', event =>{
-  const target = event.target;
-  if (target.classList.contains("delete")) {
-    let parentElement = target.parentElement;
-    parentElement.remove();
-  } else if (target.tagName === "LABEL") {
-    let text = target.innerText
-    addItem(text)
-    target.parentElement.remove();
-  }
-})
-
 //函式們////////////////////////////////////////////
-//判斷是否輸入文字的函式 (被 trim() 取代)
+//判斷是否輸入文字的函式
 function checktext (text) {
   input.classList.remove('border-danger')
   input.placeholder = 'add item'
-    let allLength = text.length
-    let wordLength = text.split(' ').length - 1 
-    if (allLength === wordLength) {
+    const textLength = text.trim().length 
+    if (textLength === 0) {
       input.classList.add('border-danger')
+      input.value = null
       input.placeholder = 'Empty item'
         return false
     } else {
@@ -88,13 +79,4 @@ function addItem (text) {
   input.value = ''
 }
 
-//加入完成的代辦清單函式
-function addFinshItem (text) {
-  const newItem = document.createElement("li");
-  newItem.innerHTML = `
-    <label for="todo" class="checked">${text}</label>
-    <i class="delete fa fa-trash"></i>
-  `;
- finshList.appendChild(newItem);
-}
 
